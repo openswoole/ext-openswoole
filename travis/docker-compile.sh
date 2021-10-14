649ca0da2eb39baca44abb16bd1466762af9af21
Bruce Dou <doubaokun@gmail.com>
2021-10-14 11:25:15 +0100

    Revert "Remove usles travis"
    This reverts commit ea7f74a76cac4a0687ad7d8f35207ca9920db3b1.

---
+++ travis/docker-compile.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+__CURRENT__=$(pwd)
+__DIR__=$(cd "$(dirname "$0")";pwd)
+
+if [ ! -f "/.dockerenv" ]; then
+    echo "" && echo "❌ This script is just for Docker env!"
+    exit
+fi
+
+#-----------compile------------
+#-------print error only-------
+cd "${__DIR__}" && cd ../ && \
+./clear.sh > /dev/null && \
+phpize --clean > /dev/null && \
+phpize > /dev/null && \
+./configure \
+--enable-openssl \
+--enable-http2 \
+--enable-sockets \
+--enable-mysqlnd \
+--enable-swoole-json \
+--enable-swoole-curl \
+--enable-cares \
+> /dev/null && \
+make -j8 > /dev/null | tee /tmp/compile.log && \
+(test "`cat /tmp/compile.log`"x = ""x || exit 255) && \
+make install && echo "" && \
+docker-php-ext-enable swoole && \
+php --ri curl && \
+php --ri swoole
+
---
+++ travis/run-tests.sh
@@ -0,0 +1,82 @@
+#!/bin/sh -e
+__CURRENT__=`pwd`
+__DIR__=$(cd "$(dirname "$0")";pwd)
+
+[ -z "${SWOOLE_BRANCH}" ] && export SWOOLE_BRANCH="master"
+
+#-------------PHPT-------------
+cd ${__DIR__} && cd ../tests/
+
+# initialization
+echo "" && echo "⭐️ Initialization for tests..." && echo ""
+./init
+cd ./include/lib
+composer update
+cd -
+echo ""
+
+# debug
+for debug_file in ${__DIR__}/debug/*.php
+do
+    if test -f "${debug_file}";then
+        debug_file_basename="`basename ${debug_file}`"
+        echo "" && echo "====== RUN ${debug_file_basename} ======" && echo ""
+        php "${debug_file}"
+        echo "" && echo "========================================" && echo ""
+    fi
+done
+
+# run tests @params($1=list_file, $2=options)
+run_tests(){
+    ./start.sh \
+    "`tr '\n' ' ' < ${1} | xargs`" \
+    -w ${1} \
+    ${2}
+}
+
+has_failures(){
+    cat tests.list
+}
+
+should_exit_with_error(){
+    if [ "${SWOOLE_BRANCH}" = "valgrind" ]; then
+        set +e
+        find ./ -type f -name "*.mem"
+        set -e
+    else
+        has_failures
+    fi
+}
+
+touch tests.list
+trap "rm -f tests.list; echo ''; echo '⌛ Done on '`date "+%Y-%m-%d %H:%M:%S"`;" EXIT
+
+cpu_num="$(/usr/bin/env php -r "echo swoole_cpu_num() * 2;")"
+options="-j${cpu_num}"
+
+echo "" && echo "🌵️️ Current branch is ${SWOOLE_BRANCH}" && echo ""
+if [ "${SWOOLE_BRANCH}" = "valgrind" ]; then
+    dir="base"
+    options="${options} -m"
+else
+    dir="swoole_*"
+fi
+echo "${dir}" > tests.list
+for i in 1 2 3 4 5
+do
+    if [ "`has_failures`" ]; then
+        if [ ${i} -gt "1" ]; then
+            sleep ${i}
+            echo "" && echo "😮 Retry failed tests#${i}:" && echo ""
+        fi
+        cat tests.list
+        timeout=`echo | expr ${i} \* 15 + 15`
+        options="${options} --set-timeout ${timeout}"
+        run_tests tests.list "${options}"
+    else
+        break
+    fi
+done
+if [ "`should_exit_with_error`" ]; then
+    exit 255
+fi
