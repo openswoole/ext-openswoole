866befa6e4c6da0858555b1e412fa9e8caf0b5e5
Hailong Zhao <hailongzh@hotmail.com>
2021-10-28 20:59:40 -0400

    Fix the stub for swoole_table::column() and swoole_table::get() method.

--- ext-src/swoole_table.stub.php
+++ ext-src/swoole_table.stub.php
@@ -4,11 +4,11 @@
 namespace Swoole {
 	final class Table {
 		public function __construct(int $table_size, ?float $conflict_proportion = 1.0) {}
-		public function column(string $name, int $type, ?int $size): void {}
+		public function column(string $name, int $type, ?int $size = 0): bool {}
 		public function create(): bool {}
 		public function destroy(): bool {}
 		public function set(string $key, array $value): bool {}
-		public function get(string $key, string $column): array|bool|string {}
+		public function get(string $key, ?string $column = null): array|bool|string {}
 		public function count(): int {}
 		public function del(string $key): bool {}
 		public function exists(string $key): bool {}
