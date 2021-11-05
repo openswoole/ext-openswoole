74ac053233ecf4f8e5022e7ca006aaf12567bd1f
Bruce Dou <doubaokun@gmail.com>
2021-11-05 14:43:31 +0000

    Added http reqeust stubs

---
+++ ext-src/swoole_http_request.stub.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ +----------------------------------------------------------------------+
+ | Open Swoole                                                          |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2021-now Open Swoole Group                             |
+ +----------------------------------------------------------------------+
+ | This source file is subject to version 2.0 of the Apache license,    |
+ | that is bundled with this package in the file LICENSE, and is        |
+ | available through the world-wide-web at the following url:           |
+ | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+ | If you did not receive a copy of the Apache2.0 license and are unable|
+ | to obtain it through the world-wide-web, please send a note to       |
+ | hello@swoole.co.uk so we can mail you a copy immediately.            |
+ +----------------------------------------------------------------------+
+*/
+
+/** @not-serializable */
+namespace Swoole\Http {
+	final class Request {
+		public function create(?array $options): mixed {}
+		public function parse(string $data): int {}
+		public function isCompleted(): bool {}
+		public function getMethod(): bool|string {}
+		public function rawContent(): bool|string {}
+		public function getContent(): bool|string {}
+		public function getData(): mixed {}
+		public function __destruct(): void {}
+
+	}
+}
---
+++ ext-src/swoole_http_request_arginfo.h
@@ -0,0 +1,26 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 7c8a4ad460565584c5bdf53fba5ee5d26bf3ca99 */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Http_Request_create, 0, 1, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO(0, options, IS_ARRAY, 1)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Http_Request_parse, 0, 1, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Http_Request_isCompleted, 0, 0, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_class_Swoole_Http_Request_getMethod, 0, 0, MAY_BE_BOOL|MAY_BE_STRING)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Http_Request_rawContent arginfo_class_Swoole_Http_Request_getMethod
+
+#define arginfo_class_Swoole_Http_Request_getContent arginfo_class_Swoole_Http_Request_getMethod
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Http_Request_getData, 0, 0, IS_MIXED, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Http_Request___destruct, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
