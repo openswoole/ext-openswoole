74c982c5db2151745e8d197a0d3359f78b2ae69f
Bruce Dou <doubaokun@gmail.com>
2021-11-11 22:21:08 +0000

    Added co\socket & server

---
+++ ext-src/swoole_server_arginfo.h
@@ -0,0 +1,185 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 1f9bf7b8bde91d052139a585ea2605af3ab05a65 */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Swoole_Server___construct, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, host, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, port, IS_LONG, 1, "0")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, mode, IS_LONG, 1, "SWOOLE_PROCESS")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, sock_type, IS_LONG, 1, "SWOOLE_SOCK_TCP")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Swoole_Server___destruct, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_listen, 0, 3, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO(0, host, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, port, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, sock_type, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_addlistener arginfo_class_Swoole_Server_listen
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_on, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, event_name, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, callback, IS_CALLABLE, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_getCallback, 0, 1, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO(0, event_name, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_set, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, settings, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_start, 0, 0, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_send, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, server_socket, IS_LONG, 1, "-1")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_sendto, 0, 3, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, ip, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, port, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, server_socket, IS_LONG, 1, "-1")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_sendwait, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_exists, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_exist arginfo_class_Swoole_Server_exists
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_protect, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, is_protected, _IS_BOOL, 1, "true")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_sendfile, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, offset, IS_LONG, 1, "0")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, length, IS_LONG, 1, "0")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_close, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, reset, _IS_BOOL, 1, "false")
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_confirm arginfo_class_Swoole_Server_exists
+
+#define arginfo_class_Swoole_Server_pause arginfo_class_Swoole_Server_exists
+
+#define arginfo_class_Swoole_Server_resume arginfo_class_Swoole_Server_exists
+
+#define arginfo_class_Swoole_Server_reload arginfo_class_Swoole_Server_start
+
+#define arginfo_class_Swoole_Server_shutdown arginfo_class_Swoole_Server_start
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_task, 0, 3, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, worker_id, IS_LONG, 1, "-1")
+	ZEND_ARG_TYPE_INFO(0, finish_callback, IS_CALLABLE, 1)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_taskwait, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, timeout, IS_DOUBLE, 1, "0.5")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, worker_id, IS_LONG, 1, "-1")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_taskWaitMulti, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, tasks, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, timeout, IS_DOUBLE, 1, "0.5")
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_taskCo arginfo_class_Swoole_Server_taskWaitMulti
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_finish, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, data, IS_MIXED, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_stop, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, worker_id, IS_LONG, 1)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, wait_reactor, _IS_BOOL, 1, "false")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_getLastError, 0, 0, IS_MIXED, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_heartbeat, 0, 0, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, close_connection, _IS_BOOL, 1, "false")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_class_Swoole_Server_getClientInfo, 0, 1, MAY_BE_BOOL|MAY_BE_ARRAY)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, reactor_id, IS_LONG, 1, "-1")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, dont_check_connection, _IS_BOOL, 1, "false")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_class_Swoole_Server_getClientList, 0, 0, MAY_BE_BOOL|MAY_BE_ARRAY)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, start_fd, IS_LONG, 1, "0")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, total, IS_LONG, 1, "10")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_getWorkerId, 0, 0, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_getWorkerPid arginfo_class_Swoole_Server_getWorkerId
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_class_Swoole_Server_getWorkerStatus, 0, 0, MAY_BE_BOOL|MAY_BE_LONG)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, worker_id, IS_LONG, 1, "-1")
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_getManagerPid arginfo_class_Swoole_Server_getWorkerId
+
+#define arginfo_class_Swoole_Server_getMasterPid arginfo_class_Swoole_Server_getWorkerId
+
+#define arginfo_class_Swoole_Server_connection_info arginfo_class_Swoole_Server_getClientInfo
+
+#define arginfo_class_Swoole_Server_connection_list arginfo_class_Swoole_Server_getClientList
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_sendMessage, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, message, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, worker_id, IS_LONG, 0, "-1")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_class_Swoole_Server_addProcess, 0, 1, MAY_BE_BOOL|MAY_BE_LONG)
+	ZEND_ARG_OBJ_INFO(0, process, Swoole\\Process, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_stats, 0, 0, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_getSocket, 0, 0, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, port, IS_LONG, 0, "-1")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_bind, 0, 2, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, fd, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, uid, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_after, 0, 2, IS_VOID, 0)
+	ZEND_ARG_TYPE_INFO(0, ms, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, callback, IS_CALLABLE, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Swoole_Server_tick arginfo_class_Swoole_Server_after
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_clearTimer, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_Swoole_Server_defer, 0, 1, IS_VOID, 0)
+	ZEND_ARG_TYPE_INFO(0, callback, IS_CALLABLE, 0)
+ZEND_END_ARG_INFO()
--- ext-src/swoole_socket_coro.stub.php
+++ ext-src/swoole_socket_coro.stub.php
@@ -15,4 +15,38 @@
  +----------------------------------------------------------------------+
 */
 
-// TODO
+/** @not-serializable */
+namespace Swoole\Coroutine {
+    final class Socket {
+        public function __construct(int $domain, int $type, ?int $protocol = IPPROTO_IP) {}
+        public function bind(string $address, ?int $port = 0): bool {}
+        public function listen(?int $backlog = 512): bool {}
+        public function accept(?float $timeout = 0): bool {}
+        public function connect(string $host, ?int $port = 0, ?float $timeout = 0) {}
+        public function checkLiveness(): bool {}
+        public function peek(?int $length = 65536): bool|string {}
+        public function recv(?int $length = 65536, ?float $timeout = 0): bool|string {}
+        public function recvAll(?int $length = 65536, ?float $timeout = 0): bool|string {}
+        public function recvLine(?int $length = 65536, ?float $timeout = 0): bool|string {}
+        public function recvWithBuffer(?int $length = 65536, ?float $timeout = 0): bool|string {}
+        public function recvPacket(?float $timeout = 0): bool|string {}
+        public function send(string $data, ?float $timeout = 0): bool|int {}
+        public function readVector(array $io_vector, ?float $timeout = 0): bool|array {}
+        public function readVectorAll(array $io_vector, ?float $timeout = 0): bool|array {}
+        public function writeVector(array $io_vector, ?float $timeout = 0): bool|int {}
+        public function writeVectorAll(array $io_vector, ?float $timeout = 0): bool|int {}
+        public function sendFile(string $filename, ?int $offset = 0, ?int $length = 0): bool {}
+        public function sendAll(string $data, ?float $timeout = 0): bool|int {}
+        public function recvfrom(mixed $peername, ?float $timeout = 0): bool|string {}
+        public function sendto(string $addr, int $port, string $data): int|bool {}
+        public function getOption(int $level, int $opt_name): mixed {}
+        public function setProtocol(array $settings): bool {}
+        public function setOption(int $level, int $opt_name, mixed $opt_value): bool {}
+        public function sslHandshake(): bool {}
+        public function shutdown(?int $how = 0): bool {}
+        public function cancel(?int $event): bool {}
+        public function close(): bool {}
+        public function getpeername(): bool|array {}
+        public function getsockname(): bool|array {}
+    }
+}
