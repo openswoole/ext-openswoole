29a81813f0794f495da3581936251b12a139709f
Bruce Dou <doubaokun@gmail.com>
2021-11-22 22:47:50 +0000

    4.8.0

--- src/server/process.cc
+++ src/server/process.cc
@@ -315,7 +315,10 @@ bool ProcessFactory::finish(SendData *resp) {
         conn = server_->get_connection_verify_no_ssl(session_id);
     }
     if (!conn) {
-        swoole_error_log(SW_LOG_NOTICE, SW_ERROR_SESSION_NOT_EXIST, "session#%ld does not exists, it may be closed by the other side", session_id);
+        swoole_error_log(SW_LOG_NOTICE,
+                         SW_ERROR_SESSION_NOT_EXIST,
+                         "session#%ld does not exists, it may be closed by the other side",
+                         session_id);
         return false;
     } else if ((conn->closed || conn->peer_closed) && resp->info.type != SW_SERVER_EVENT_CLOSE) {
         swoole_error_log(SW_LOG_NOTICE,
