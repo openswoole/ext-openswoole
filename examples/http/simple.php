bea32b0c748e7cbc7485fc3cd1e841036c2bcf72
Bruce Dou <doubaokun@gmail.com>
2021-12-19 16:47:08 +0000

    Updated example

--- examples/http/simple.php
+++ examples/http/simple.php
@@ -4,7 +4,40 @@ use Swoole\Http\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 
+$table = new Swoole\Table(1024);
+$table->column('name', Swoole\Table::TYPE_STRING, 64);
+$table->column('id', Swoole\Table::TYPE_INT, 4);       //1,2,4,8
+$table->column('num', Swoole\Table::TYPE_FLOAT);
+$table->create();
+
+$table1 = new Swoole\Table(1024);
+$table1->column('name', Swoole\Table::TYPE_STRING, 64);
+$table1->column('id', Swoole\Table::TYPE_INT, 4);       //1,2,4,8
+$table1->column('num', Swoole\Table::TYPE_FLOAT);
+$table1->create();
+
 $server = new Swoole\HTTP\Server("127.0.0.1", 9501);
+$server->set([
+    'worker_num' => 4,
+    'task_worker_num' => 4,
+    //'max_request' => 10000,
+    //'max_request_grace' => 0,
+]);
+
+$process = new Swoole\Process(function($process) use ($server) 
+{
+    while(true) 
+    {
+        $msg = $process->read();
+
+        foreach($server->connections as $conn) 
+        {
+            $server->send($conn, $msg);
+        }
+    }
+});
+
+$server->addProcess($process);
 
 $server->on("Start", function(Server $server)
 {
@@ -12,16 +45,37 @@ $server->on("Start", function(Server $server)
 });
 
 class A {
-    public $a = 'abcd';
+    public string $a = '';
+
+    function __construct() {
+        $this->a = str_repeat('abcd', 1000);
+    }
 };
 
 $server->on("Request", function(Request $request, Response $response) use ($server)
 {
-    // memory leak example
-    global $c;
-    $c[] = new A();
+    // var_dump($request);
+    // // memory leak example
+    // global $c;
+    // $c[] = new A();
+    // global $d;
+    // $d[] = 'a';
     $response->header("Content-Type", "text/plain");
-    $response->end(json_encode($server->stats()));
+    //return $response->end($server->stats(\SWOOLE_STATS_MODE_OPENMETRICS));
+    print_r($server->stats());
+    return $response->end();
+
+});
+
+
+
+$server->on('Task', function (Swoole\Server $server, $task_id, $reactorId, $data)
+{
+    echo "Task Worker Process received data";
+
+    echo "#{$server->worker_id}\tonTask: [PID={$server->worker_pid}]: task_id=$task_id, data_len=" . strlen($data) . "." . PHP_EOL;
+
+    $server->finish($data);
 });
 
 $server->start();
