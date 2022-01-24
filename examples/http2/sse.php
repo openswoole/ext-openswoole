f210200872bff15436bb4e416545f6de4cc1a499
Bruce Dou <doubaokun@gmail.com>
2022-01-24 10:53:01 +0000

    Added HTTP2 & SSE example

--- examples/http2/sse.php
+++ examples/http2/sse.php
@@ -3,12 +3,15 @@ Swoole\Coroutine::set([
     'trace_flags' => SWOOLE_TRACE_HTTP2,
     'log_level' => 0,
 ]);
-$http = new swoole_http_server("0.0.0.0", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+$key_dir = __DIR__ . '/../ssl/';
+$http = new swoole_http_server("0.0.0.0", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP | SWOOLE_SSL);
 $http->set([
     'open_http2_protocol' => 1,
     'enable_static_handler' => TRUE,
-    'document_root' => dirname(__DIR__),
+    'document_root' => __DIR__,
     'enable_coroutine' => true,
+    'ssl_cert_file' => $key_dir . '/example.com+4.pem',
+    'ssl_key_file' => $key_dir . '/example.com+4-key.pem',
 ]);
 
 $http->on('request', function (swoole_http_request $request, swoole_http_response $response) {
@@ -18,13 +21,13 @@ $http->on('request', function (swoole_http_request $request, swoole_http_respons
     $response->header('X-Accel-Buffering', 'no');
 
     while(1) {
-        $response->write("event: ping\n");
+        $response->write("event: ping\n\n");
         $r = $response->write("data: {\"time\": \"" . date(DATE_ISO8601) . "\"}\n\n");
         if(!$r) return;
         co::sleep(1);
     }
     
-    $response->end("<h1>Hello Swoole</h1>");
+    $response->end(__DIR__);
 });
 
 $http->start();
