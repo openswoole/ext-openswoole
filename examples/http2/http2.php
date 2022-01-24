a072f9319ec5ebcd37f35888ad11eabd6d5eb749
Bruce Dou <doubaokun@gmail.com>
2022-01-24 18:14:38 +0000

    Updated http2 example

--- examples/http2/http2.php
+++ examples/http2/http2.php
@@ -4,6 +4,7 @@ $http = new swoole_http_server("127.0.0.1", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP |
 $http->set([
     'open_http2_protocol' => 1,
     'enable_static_handler' => TRUE,
+    'enable_coroutine' => TRUE,
     'document_root' => __DIR__,
     'ssl_cert_file' => $key_dir . '/example.com+4.pem',
     'ssl_key_file' => $key_dir . '/example.com+4-key.pem',
