9f17b810ca1366aa4e50481656acbd78f3691ef4
Bruce Dou <doubaokun@gmail.com>
2022-08-31 17:59:50 +0100

    clean up useless files

--- examples/atomic/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$atomic = new Swoole\Atomic(123);
-echo $atomic->add(12)."\n";
-echo $atomic->sub(11)."\n";
-echo $atomic->cmpset(122, 999)."\n";
-echo $atomic->cmpset(124, 999)."\n";
-echo $atomic->get()."\n";
--- examples/coroutine/scheduler.php
+++
@@ -1,5 +0,0 @@
-<?php
-Co\Run(function () {
-    Co::sleep(0.2);
-    echo "hello world\n";
-});
