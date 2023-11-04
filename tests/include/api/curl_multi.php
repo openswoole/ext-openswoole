8c8a90aeb653337d8c98e843a28babf813aa80cb
Bruce Dou <doubaokun@gmail.com>
2023-11-04 14:02:47 +0000

    fix: ci

--- tests/include/api/curl_multi.php
+++ tests/include/api/curl_multi.php
@@ -3,12 +3,11 @@ function swoole_test_curl_multi($options = []) {
     $ch1 = curl_init();
     $ch2 = curl_init();
 
-    // 设置URL和相应的选项
-    curl_setopt($ch1, CURLOPT_URL, "http://www.baidu.com/");
+    curl_setopt($ch1, CURLOPT_URL, "https://www.google.com/");
     curl_setopt($ch1, CURLOPT_HEADER, 0);
     curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
 
-    curl_setopt($ch2, CURLOPT_URL, "http://www.gov.cn/");
+    curl_setopt($ch2, CURLOPT_URL, "https://openswoole.com/");
     curl_setopt($ch2, CURLOPT_HEADER, 0);
     curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
 
@@ -18,7 +17,6 @@ function swoole_test_curl_multi($options = []) {
     curl_multi_add_handle($mh, $ch2);
 
     $active = null;
-    // 执行批处理句柄
     do {
         $mrc = curl_multi_exec($mh, $active);
     } while ($mrc == CURLM_CALL_MULTI_PERFORM);
@@ -54,8 +52,8 @@ function swoole_test_curl_multi($options = []) {
     Assert::eq($info2['msg'], CURLMSG_DONE);
     Assert::eq($info3, false);
 
-    Assert::contains(curl_multi_getcontent($ch1), 'baidu.com');
-    Assert::contains(curl_multi_getcontent($ch2), '中央人民政府门户网站');
+    Assert::contains(curl_multi_getcontent($ch1), 'google');
+    Assert::contains(curl_multi_getcontent($ch2), 'OpenSwoole');
 
     curl_multi_remove_handle($mh, $ch1);
     curl_multi_remove_handle($mh, $ch2);
